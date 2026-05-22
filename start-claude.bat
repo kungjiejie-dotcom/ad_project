a71e7d691017c22777b2c75f3ca56c75a99f0ccf
kungjiejie <kungjiejie@gmail.com>
2026-05-22 11:05:54 +0800

    Delete start-claude.bat

--- start-claude.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set ANTHROPIC_BASE_URL=https://api.deepseek.com/anthropic
-set ANTHROPIC_AUTH_TOKEN=sk-006a5ec0ab9c4618a0e722cee1ea4435
-set ANTHROPIC_MODEL=deepseek-v4-pro[1m]
-set ANTHROPIC_SMALL_FAST_MODEL=deepseek-v4-flash
-claude
